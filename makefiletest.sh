8d3229e07f56d7918a279022677b3db63b7a105d
helperLin <152912947+helperLin@users.noreply.github.com>
2023-12-04 13:41:01 -0500

    Update makefiletest.sh

--- makefiletest.sh
+++ makefiletest.sh
@@ -1,8 +1,8 @@
-https://powcoder.com
-代写代考加微信 powcoder
-Assignment Project Exam Help
-Add WeChat powcoder
-#!/bin/bash
+# CIT 593 HW13 dynamic memory答案
+# CIT 5930 Final Project期末大作业参考答案
+# 不加WeChat，Email联系: helpLin101@outlook.com
+# 原创唯一 完全保密 保证质量 性价比高
+
 
 remove_compiled () {
     rm -f autograder*
